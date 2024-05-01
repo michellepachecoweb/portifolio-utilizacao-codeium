c4fc6b4abb620257aa0682a0cfefc2c3e75f755e
michellepachecoweb <michellepachecoweb@gmail.com>
2024-05-01 11:48:58 -0300

    feat: adicionar,  listar itens e limpeza formulário

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,54 @@ import './App.css'
 // this compon ent renders a todo list, with task name, completion state and uses the ID as key
 
 type Todo = {
-  id: string
+  id: number
   name: string
   completed: boolean
 }
 
-function App() {
-   
+function App() {   
   const [todoList, setTodoList] = useState<Todo[]>([])
 
+// this function adds the event target to the todoList array
+  const addTodo = (event: React.FormEvent) => {
+      event.preventDefault()
+      const target = event.target as typeof event.target & {
+        todo: { value: string }
+      }
+      const name = target.todo.value
+      setTodoList([...todoList, {
+        id: todoList.length + 1,
+        name: name,
+        completed: false
+      }])
+      // clear the input field after adding the todo
+      target.todo.value = ''
+    }
+
   return (
-  
+    <>
         <div className='App'>
           <h1>Todo List</h1>
+            {/* 
+                implement an input field to add new tasks
+                implement a button to add the new task
+            */}
+              <div>
+                <form onSubmit={addTodo} > 
+                  <input type="text" name='todo'/>
+                  <button>Add</button>
+                </form>
+              </div>
+
+
+
+
               {todoList.map((todo) => (
                 <div key={todo.id}> {todo.name} </div>
               ))}
         </div>
      
-    
+    </>
   )
 }
 
